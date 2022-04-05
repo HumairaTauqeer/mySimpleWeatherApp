a70bf952be88c4beb82cfa41275da5b1ae4ae513
Humaira Tauqeer <humaira.tauqeer87@gmai.com>
2022-04-05 11:51:03 -0400

    grab html elements

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,20 @@
-const apiKey = process.env.OPEN_WEATHER_API_KEY;
+const apiKey = '5a42fdfc85e4a33bf159cfbc35b3bb2c';
+
+const form = document.getElementById('form');
+
+const submitButton = document.getElementById('button');
+
+const errorDiv = document.querySelector('.error-message');
+const ulList = document.querySelector('.display-weather .cities');
+
+
+
+submitButton.onclick = (e) => {
+    e.preventDefault()
+    const input = document.getElementById('input');
+    const locationInput = input.value;
+    // console.log(locationInput);
+    const requestUrl = `https://api.openweathermap.org/data/2.5/weather?q=${locationInput}&appid=${apiKey}&units=imperial`
+
+
+}
